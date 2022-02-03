2161cd172443c769544db30b2a67a3abacfbe57f
Henrycious <henry.gennet@gmail.com>
2022-02-03 16:39:30 +0100

    find camera by name
    changed finding camera by framesize to finding camera by name ('USB Camera: USB Camera')

--- thermal_cam/publish_thermal_stream.py
+++ thermal_cam/publish_thermal_stream.py
@@ -7,6 +7,8 @@ from std_msgs.msg import String
 from sensor_msgs.msg import Image
 import matplotlib.pyplot as plt
 import cv2
+import os
+import re
 import numpy as np
 
 
@@ -17,40 +19,37 @@ class ThermalPublisher(Node):
         self.publisher2_ = self.create_publisher(Image,'thermal_colormap',10)
         timer_period = 1/60; #camera runs with 60 FPS
         self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.cam_found = False
-        if not self.cam_found:
-            for i in range(20):
-                self.cap = cv2.VideoCapture(i)
-                success, img = self.cap.read()
-                if success: 
-                    height, width = img.shape[:2]
-                    if height == 384 and width == 256:
-                        self.cam_found = True           
-                        self.vid_channel=i
-                        break
-                    else:
-                        continue
-                else:
-                    continue
-            
+
+        cam_number = self.get_camera_index_by_name('USB Camera: USB Camera')
+        self.cap = cv2.VideoCapture(cam_number)
+
         self.cvBridge = CvBridge()
 
     def timer_callback(self):
         ret, frame = self.cap.read()
-        colormap = plt.get_cmap('inferno')
-        top_frame = frame[0:192,0:256]
-        heatmap = cv2.applyColorMap(top_frame, cv2.COLORMAP_JET)
-
-
+        
         if ret:
+            colormap = plt.get_cmap('inferno')
+            top_frame = frame[0:192,0:256]
+            heatmap = cv2.applyColorMap(top_frame, cv2.COLORMAP_JET)
             self.publisher_.publish(self.cvBridge.cv2_to_imgmsg(top_frame))
             self.publisher2_.publish(self.cvBridge.cv2_to_imgmsg(heatmap, encoding='bgr8'))
-        
-      
 
-        # cv2.imshow("camera", top_frame)
-        # cv2.imshow("heatmap", heatmap)
-        # cv2.waitKey(1)
+    def get_camera_index_by_name(camera_name):
+            cam_num = None
+            for file in os.listdir("/sys/class/video4linux"):
+                real_file = os.path.realpath("/sys/class/video4linux/" + file + "/name")
+                with open(real_file, "rt") as name_file:
+                    name = name_file.read().rstrip()
+                if camera_name in name:
+                    cam_num = int(re.search("\d+$", file).group(0))
+                    found = "FOUND!"
+                    return cam_num
+                    break
+                else:
+                    found = "      "
+                print("{} {} -> {}".format(found, file, name))
+            return cam_num
 
 def main(args=None):
     
